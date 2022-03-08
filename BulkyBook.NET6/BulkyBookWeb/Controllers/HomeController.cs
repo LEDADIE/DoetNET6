271c51d6b91f45c97d1de2c58391b5a7a5e600c8
Levry Eugene Dadie <dadielevry15@outlook.com>
2022-03-08 15:32:59 +0000

    Ajoutez des fichiers projet.

---
+++ BulkyBook.NET6/BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBookWeb.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Help()
+        {
+            return View("HelpView");
+        }
+
+        public IActionResult Index()
+        {
+            //_logger.LogInformation("Demarrage de l'application");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyBook.NET6/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    [DisplayName("Display order")]
+    [Range(1, 100, ErrorMessage = "Display order mus be between 1 and 100 only !!")]
+    public int Displayorder { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+}
